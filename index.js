01a3480962ed149b20f4a63b8e02ba6de920702d
Gabriel Aguirre <gabriel.aguirre@bayard-consulting.com>
2020-05-09 18:15:20 +0200

    react ist dabei

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ function createWindow () {
 }
 
 app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // Unter macOS ist es üblich, für Apps und ihre Menu Bar
+    // aktiv zu bleiben, bis der Nutzer explizit mit Cmd + Q die App beendet.
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // Unter macOS ist es üblich ein neues Fenster der App zu erstellen, wenn
+    // das Dock Icon angeklickt wird und keine anderen Fenster offen sind.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
