50233f33ad70bda0fdc46471fe744db69224c140
Tobias Trude <tobiastrude@Tobiass-MacBook-Pro.local>
2020-05-23 19:12:52 +0200

    live server and stuff works now yay

--- hackbois-react/src/components/app/App.js
+++ hackbois-react/src/components/app/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <div>
         Listen Sie hier alle Gründe auf, warum Gabriel und Karl auch ohne Matthias und Tobi zurecht kommen:
         </div>
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -27,6 +27,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9000
+    port: 9000,
+    liveReload: true
   }
 };
